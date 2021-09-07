516eb94aedea336c5035af830aa0a51a82073668
sukhae <sukhae@gmail.com>
2021-09-07 23:53:03 +0900

    메인화면 업그레이드 및 회원가입ㅋ하고 다시 커밋ㅋ

--- app/src/main/java/org/techtown/letseat/MainActivity.java
+++ app/src/main/java/org/techtown/letseat/MainActivity.java
@@ -16,7 +16,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_res_list);
+        setContentView(R.layout.activity_main);
 
         Button btnQR = findViewById(R.id.btnQR);
 
