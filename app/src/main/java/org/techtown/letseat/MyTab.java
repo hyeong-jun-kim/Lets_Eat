cf3a2708de9952d7b8248816697ecd137e1c6de5
taehyun24 <ahnth0316@naver.com>
2021-09-12 23:16:42 +0900

    09.12 work

---
+++ app/src/main/java/org/techtown/letseat/MyTab.java
@@ -0,0 +1,14 @@
+package org.techtown.letseat;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MyTab extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_tab);
+    }
+}
