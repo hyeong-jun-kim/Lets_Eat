2d4ae9f4d382ceadedf87acb89d5603b7fcf4a01
sukhae <sukhae@gmail.com>
2021-09-21 23:51:02 +0900

    회원가입ㅋ

--- app/src/main/java/org/techtown/letseat/login/Login.java
+++ app/src/main/java/org/techtown/letseat/login/Login.java
@@ -12,18 +12,26 @@ import org.techtown.letseat.R;
 import org.techtown.letseat.restaurant.list.RestListMain;
 
 public class Login extends AppCompatActivity {
-    Button button;
+    Button btnLog, btnRegister;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_activity);
 
-        button = findViewById(R.id.review_button);
+        btnLog = findViewById(R.id.login_button);
+        btnRegister = findViewById(R.id.btnRegister);
 
-        button.setOnClickListener(new View.OnClickListener() {
+        btnLog.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+            }
+        });
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), RegisterActivity.class);
                 startActivity(intent);
             }
         });
