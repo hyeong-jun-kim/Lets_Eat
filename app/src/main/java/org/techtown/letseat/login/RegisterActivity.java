e397a57d29c597a8776ffa89dc75966de943d94c
forceTlight <53989167+forceTlight@users.noreply.github.com>
2021-09-24 01:20:34 +0900

    9.24 register & login function generate

--- app/src/main/java/org/techtown/letseat/login/RegisterActivity.java
+++ app/src/main/java/org/techtown/letseat/login/RegisterActivity.java
@@ -3,6 +3,7 @@ package org.techtown.letseat.login;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.DialogFragment;
 
+import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.text.Editable;
@@ -15,7 +16,17 @@ import android.widget.RadioGroup;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
 import org.json.JSONObject;
+import org.techtown.letseat.AppHelper;
 import org.techtown.letseat.DatePickerFragment;
 import org.techtown.letseat.R;
 
@@ -24,6 +35,8 @@ import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
 
 public class RegisterActivity extends AppCompatActivity {
     private String birthday_string, email_string, pwd_string, name_string, pwd_check_string, gender;
@@ -32,7 +45,7 @@ public class RegisterActivity extends AppCompatActivity {
     private EditText email, pwd, name, pwd_check;
     private RadioGroup gender_radio;
     private RadioButton male, female;
-    Button btn_register, email_check, register_input;
+    Button btn_register, btn_email_check, btn_birthday;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,20 +59,24 @@ public class RegisterActivity extends AppCompatActivity {
         gender_radio = findViewById(R.id.register_gender);
         male = findViewById(R.id.register_male);
         female = findViewById(R.id.register_female);
-        register_input = findViewById(R.id.register_birthday_input);
-        register_input.setOnClickListener(new View.OnClickListener() {
+        btn_birthday = findViewById(R.id.register_birthday_input);
+        btn_birthday.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 showDatePicker(v);
             }
         });
-        email_check = findViewById(R.id.register_email_check);
-        email.setOnClickListener(new View.OnClickListener() {
+        btn_email_check = findViewById(R.id.register_email_check);
+        btn_email_check.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                email_string = email.getText().toString();
+                sendLoginCheckRequest(email_string, email);
             }
         });
+        if (AppHelper.requestQueue != null) { //RequestQueue 생성
+            AppHelper.requestQueue = Volley.newRequestQueue(getApplicationContext());
+        }
         btn_register = findViewById(R.id.btn_register);
         btn_register.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -84,18 +101,17 @@ public class RegisterActivity extends AppCompatActivity {
                 } else if (!pwd_check_string.equals(pwd_string)) {
                     Toast.makeText(getApplicationContext(), "비밀번호랑 비밀번호 확인란이 일치하지 않스니다. 다시 확인해주세요.", Toast.LENGTH_SHORT).show();
                 } else {
-                    CustomTask customTask = new CustomTask();
-                    customTask.execute();
+                    sendRegisterRequest();
                 }
             }
         });
     }
-
+    // DatePicker Fragment 보여줌
     public void showDatePicker(View view) {
         DialogFragment newFragment = new DatePickerFragment();
         newFragment.show(getSupportFragmentManager(), "datePicker");
     }
-
+    // DatePicker에서 Date 선택 후 처리
     public void processDatePickerResult(int year, int month, int day) {
         String month_string = Integer.toString(month + 1);
         String day_string = Integer.toString(day);
@@ -103,59 +119,74 @@ public class RegisterActivity extends AppCompatActivity {
         birthday_string = year_string + "." + month_string + "." + day_string;
         birthday.setText(birthday_string);
     }
-
-    class CustomTask extends AsyncTask<String, Void, String> {
-        InputStream is = null;
-        String result = "";
-
-        @Override
-        protected String doInBackground(String... strings) {
-            try {
-                URL urlCon = new URL("http://220.70.169.23:8000/letseat/register/normal");
-                HttpURLConnection httpCon = (HttpURLConnection) urlCon.openConnection();
-                String json = "";
-
-                JSONObject jsonObject = new JSONObject();
-                jsonObject.accumulate("email", email_string);
-                jsonObject.accumulate("password", pwd_string);
-                jsonObject.accumulate("name", name_string);
-                jsonObject.accumulate("birthday", birthday_string);
-                jsonObject.accumulate("gender", gender);
-
-                json = jsonObject.toString();
-
-                httpCon.setRequestProperty("Accept", "application/json");
-                httpCon.setRequestProperty("Content-type", "application/json");
-
-                httpCon.setDoOutput(true);
-                httpCon.setDoInput(true);
-
-                OutputStream os = httpCon.getOutputStream();
-                os.write(json.getBytes("euc-kr"));
-                os.flush();
-                try {
-                    is = httpCon.getInputStream();
-                    if (is != null)
-                        result = "OK";
-                    else
-                        result = "BAD";
-                } catch (IOException e) {
-                    e.printStackTrace();
-                } finally {
-                    httpCon.disconnect();
+    // 회원가입 POST 요청
+    public void sendRegisterRequest() {
+        String url = "http://220.70.169.23:8000/letseat/register/normal";
+        JSONObject postData = new JSONObject();
+        try {
+            postData.put("email", email_string);
+            postData.put("password", pwd_string);
+            postData.put("birthday", birthday_string);
+            postData.put("gender", gender);
+            postData.put("name", name_string);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        JsonObjectRequest request = new JsonObjectRequest(
+                Request.Method.POST,
+                url,
+                postData,
+                new Response.Listener<JSONObject>() {
+                    @Override // 응답 잘 받았을 때
+                    public void onResponse(JSONObject response) {
+                        Intent intent = new Intent(RegisterActivity.this, Login.class);
+                        Toast.makeText(getApplicationContext(), "회원가입이 완료되었습니다.", Toast.LENGTH_SHORT).show();
+                        startActivity(intent);
+                        finish();
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override // 에러 발생 시
+                    public void onErrorResponse(VolleyError error) {
+                        println("이미 존재하는 회원입니다.");
+                    }
                 }
-            } catch (IOException e) {
-                e.printStackTrace();
-            } catch (Exception e) {
-                Log.d("InputStream", e.getLocalizedMessage());
-            }
-            Toast.makeText(getApplicationContext(), result, Toast.LENGTH_SHORT).show();
-            return result;
-        }
-        @Override
-        protected  void onPostExecute(String s){
-            Toast.makeText(getApplicationContext(), result, Toast.LENGTH_SHORT).show();
-            super.onPostExecute(s);
-        }
+        );
+        request.setShouldCache(false); // 이전 결과 있어도 새로 요청해 응답을 보내줌
+        AppHelper.requestQueue = Volley.newRequestQueue(this); // requsetQueue 초기화
+        AppHelper.requestQueue.add(request);
+    }
+
+    // 로그인 중복 확인 GET
+    public void sendLoginCheckRequest(String email_string, TextView email) {
+        String url = "http://220.70.169.23:8000/letseat/register/email/check?email=" + email_string;
+        StringRequest request = new StringRequest(
+                Request.Method.GET,
+                url,
+                new Response.Listener<String>() {
+                    @Override // 응답 잘 받았을 때
+                    public void onResponse(String response) {
+                        if (response.equals("emailCheckFail")) {
+                            println("위 이메일 주소는 사용불가능합니다.");
+                            email.setText("");
+                        } else {
+                            println("사용가능한 이메일입니다.");
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override // 에러 발생 시
+                    public void onErrorResponse(VolleyError error) {
+                        println("연결 상태 불량");
+                    }
+                }
+        );
+        request.setShouldCache(false); // 이전 결과 있어도 새로 요청해 응답을 보내줌
+        AppHelper.requestQueue = Volley.newRequestQueue(this); // requsetQueue 초기화
+        AppHelper.requestQueue.add(request);
+    }
+
+    public void println(String text) {
+        Toast.makeText(getApplicationContext(), text, Toast.LENGTH_SHORT).show();
     }
 }
