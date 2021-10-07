b59e2685396e53aac57dd0ad59c64a8575f85b42
KSB3870 <73988645+KSB3870@users.noreply.github.com>
2021-10-08 00:41:39 +0900

    풀용 커밋

--- app/src/main/java/org/techtown/letseat/Review/ReviewImage1.java
+++ app/src/main/java/org/techtown/letseat/Review/ReviewImage1.java
@@ -9,6 +9,7 @@ import android.Manifest;
 import android.content.SharedPreferences;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Color;
 import android.graphics.drawable.BitmapDrawable;
 import android.net.Uri;
 import android.provider.MediaStore;
@@ -19,8 +20,15 @@ import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
 
+import java.io.BufferedReader;
 import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.nio.FloatBuffer;
 
 import android.util.Log;
 import android.view.View;
@@ -35,6 +43,12 @@ import com.bumptech.glide.load.resource.bitmap.CenterCrop;
 import com.bumptech.glide.load.resource.bitmap.FitCenter;
 import com.bumptech.glide.load.resource.bitmap.RoundedCorners;
 import com.bumptech.glide.request.RequestOptions;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.ml.modeldownloader.CustomModel;
+import com.google.firebase.ml.modeldownloader.CustomModelDownloadConditions;
+import com.google.firebase.ml.modeldownloader.DownloadType;
+import com.google.firebase.ml.modeldownloader.FirebaseModelDownloader;
+
 import org.techtown.letseat.R;
 import org.tensorflow.lite.Interpreter;
 
@@ -43,9 +57,11 @@ public class ReviewImage1 extends AppCompatActivity {
     private Intent intent;
     private ImageView imageView;
     private Button upload_btn, save_btn;
+    private Bitmap bitmap;
+    private String photoPath, foodName, argmax;
     private TextView menu_name, accuracy;
     private SharedPreferences sharedPreferences;
-
+    private Interpreter interpreter;
     private static final String TAG = "ReviewActivity";
     private Uri filePath;
 
@@ -74,6 +90,53 @@ public class ReviewImage1 extends AppCompatActivity {
                 finish();
             }
         });
+
+        LoadTFliteModel();
+
+        // 입력 데이터에 대한 추론 수행
+        /*Bitmap bitmap = Bitmap.createScaledBitmap(bitmap, 224, 224, true);
+        ByteBuffer input = ByteBuffer.allocateDirect(224 * 224 * 3 * 4).order(ByteOrder.nativeOrder());
+        for (int y = 0; y < 224; y++) {
+            for (int x = 0; x < 224; x++) {
+                int px = bitmap.getPixel(x, y);
+
+                // Get channel values from the pixel value.
+                int r = Color.red(px);
+                int g = Color.green(px);
+                int b = Color.blue(px);
+
+                // Normalize channel values to [-1.0, 1.0]. This requirement depends
+                // on the model. For example, some models might require values to be
+                // normalized to the range [0.0, 1.0] instead.
+                float rf = (r - 127) / 255.0f;
+                float gf = (g - 127) / 255.0f;
+                float bf = (b - 127) / 255.0f;
+
+                input.putFloat(rf);
+                input.putFloat(gf);
+                input.putFloat(bf);
+            }
+        }
+
+        // 출력을 포함할 수 있도록 ByteBuffer에 크기 할당, 출력버퍼를 인터프리터의 run() 메서드에 전달
+        int bufferSize = 150 * java.lang.Float.SIZE / java.lang.Byte.SIZE;
+        ByteBuffer modelOutput = ByteBuffer.allocateDirect(bufferSize).order(ByteOrder.nativeOrder());
+        interpreter.run(input, modelOutput);
+
+        // 출력 사용 방법(색인을 색인이 나타내는 라벨에 매핑 가능)
+        modelOutput.rewind();
+        FloatBuffer probabilities = modelOutput.asFloatBuffer();
+        try {
+            BufferedReader reader = new BufferedReader(
+                    new InputStreamReader(getAssets().open("custom_labels.txt")));
+            for (int i = 0; i < probabilities.capacity(); i++) {
+                String label = reader.readLine();
+                float probability = probabilities.get(i);
+                Log.i(TAG, String.format("%s: %1.4f", label, probability));
+            }
+        } catch (IOException e) {
+            // File not found?
+        }*/
     }
 
     @Override
@@ -123,5 +186,28 @@ public class ReviewImage1 extends AppCompatActivity {
                     break;
             }
         }
+    }
+
+    public void LoadTFliteModel(){
+        // 모델 배포(기기에 모델 다운로드 및 TFLite 인터프리터 초기화)
+        CustomModelDownloadConditions conditions = new CustomModelDownloadConditions.Builder()
+                .requireWifi()  // Also possible: .requireCharging() and .requireDeviceIdle()
+                .build();
+        FirebaseModelDownloader.getInstance()
+                .getModel("Food-Detector", DownloadType.LOCAL_MODEL_UPDATE_IN_BACKGROUND, conditions)
+                .addOnSuccessListener(new OnSuccessListener<CustomModel>() {
+                    @Override
+                    public void onSuccess(CustomModel model) {
+                        // Download complete. Depending on your app, you could enable the ML
+                        // feature, or switch from the local model to the remote model, etc.
+                        Log.e("TFLite Model Download","Model Success!");
+                        // The CustomModel object contains the local path of the model file,
+                        // which you can use to instantiate a TensorFlow Lite interpreter.
+                        File modelFile = model.getFile();
+                        if (modelFile != null) {
+                            interpreter = new Interpreter(modelFile);
+                        }
+                    }
+                });
     }
 }
