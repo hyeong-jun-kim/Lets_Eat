545f978b4e974dc5d5aa50df263720d00e9cdd93
forceTlight <gamebox27@naver.com>
2021-10-10 13:31:25 +0900

    10.10 rollback

--- app/src/main/java/org/techtown/letseat/restaurant/info/MenuAdapter.java
+++ app/src/main/java/org/techtown/letseat/restaurant/info/MenuAdapter.java
@@ -39,7 +39,7 @@ public class MenuAdapter extends RecyclerView.Adapter<MenuAdapter.ViewHolder>{
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         View view = LayoutInflater.from(parent.getContext())
-                .inflate(R.layout.menu_recycle, parent, false);
+                .inflate(R.layout.menu_recycler, parent, false);
         return new ViewHolder(view);
     }
 
