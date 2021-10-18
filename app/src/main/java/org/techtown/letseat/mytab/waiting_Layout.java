226c71eccf74874ebd51e234055a4203f7c772c8
forceTlight <53989167+forceTlight@users.noreply.github.com>
2021-10-18 22:28:19 +0900

    마이탭 대기현황 수정

--- app/src/main/java/org/techtown/letseat/mytab/waiting_Layout.java
+++ app/src/main/java/org/techtown/letseat/mytab/waiting_Layout.java
@@ -11,19 +11,11 @@ import org.techtown.letseat.restaurant.list.RestListAdapter;
 
 public class waiting_Layout extends AppCompatActivity {
 
-    private RestListAdapter adapter = new RestListAdapter();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.mytab_waiting);
 
-        RecyclerView recyclerView = findViewById(R.id.recycler_view);
-
-        recyclerView.setLayoutManager(new LinearLayoutManager(this));
-        recyclerView.setAdapter(adapter);
-
-        //아이템 로드
-        adapter.setItems(new waiting_status_data().getItems());
     }
 }
