b3785bcdf0d6ec2e9cba560f5571a17898886bab
sukhae <sukhae@gmail.com>
2021-10-23 00:24:42 +0900

    메인 리뷰사진 리사이클 추가

--- app/src/main/java/org/techtown/letseat/MainActivity.java
+++ app/src/main/java/org/techtown/letseat/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
+import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import androidx.viewpager2.widget.ViewPager2;
@@ -55,7 +56,9 @@ import org.techtown.letseat.mytab.MyTab;
 import org.techtown.letseat.order.OrderActivity;
 import org.techtown.letseat.order.Orderdata;
 import org.techtown.letseat.pay_test.Kakao_pay_test;
+import org.techtown.letseat.photo.PhotoData;
 import org.techtown.letseat.photo.PhotoList;
+import org.techtown.letseat.photo.PhotoRecyclerAdapter;
 import org.techtown.letseat.restaurant.info.RestInfoMain;
 import org.techtown.letseat.restaurant.list.RestListMain;
 import org.techtown.letseat.restaurant.qr.qr_restActivity;
@@ -66,6 +69,7 @@ import org.techtown.letseat.util.PhotoSave;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.Locale;
@@ -87,6 +91,9 @@ public class MainActivity extends AppCompatActivity {
     ArrayList<MainRecyclerData> items = new ArrayList<>();
     ArrayList<Double> differList = new ArrayList<>();
     ArrayList<MainRecyclerData> arrayList = new ArrayList<>();
+    private PhotoRecyclerAdapter adapter2;
+    List<Integer> listResId = Arrays.asList(R.drawable.image1, R.drawable.image2, R.drawable.image3,
+            R.drawable.menuimg1, R.drawable.menuimg2, R.drawable.menuimg3);
 
 
     private GpsTracker gpsTracker;
@@ -108,6 +115,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        init();
+        getData();
+
         checkRunTimePermission();
         if (AppHelper.requestQueue != null) { //RequestQueue 생성
             AppHelper.requestQueue = Volley.newRequestQueue(getApplicationContext());
@@ -537,5 +548,22 @@ public class MainActivity extends AppCompatActivity {
         request.setShouldCache(false);
         AppHelper.requestQueue = Volley.newRequestQueue(this);
         AppHelper.requestQueue.add(request);
+    }
+    // 처음 시작 시 리사이클러뷰 세팅하기
+    private void init() {
+        RecyclerView recyclerView2 = findViewById(R.id.mainphotoRecycler);
+        GridLayoutManager layoutManager2 = new GridLayoutManager(this, 3);
+        recyclerView2.setLayoutManager(layoutManager2);
+        adapter2 = new PhotoRecyclerAdapter();
+        recyclerView2.setAdapter(adapter2);
+    }
+
+    // 처음 시작 시 리사이클러뷰 불러오기
+    private void getData() {
+        for (int i = 0; i < listResId.size(); i++) {
+            PhotoData data = new PhotoData();
+            data.setResId(listResId.get(i));
+            adapter2.addItem(data);
+        }
     }
 }
