43f5ff1f08f18fd1ad429dccba8447c1ac3bb312
sukhae <sukhae@gmail.com>
2021-10-27 23:44:22 +0900

    디자인

--- app/src/main/java/org/techtown/letseat/idActivity.java
+++ app/src/main/java/org/techtown/letseat/idActivity.java
@@ -3,6 +3,9 @@ package org.techtown.letseat;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.appbar.MaterialToolbar;
 
 public class idActivity extends AppCompatActivity {
 
@@ -10,5 +13,14 @@ public class idActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_id);
+
+        MaterialToolbar toolbar = findViewById(R.id.topMain);
+
+        toolbar.setNavigationOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
     }
 }
--- app/src/main/java/org/techtown/letseat/passwordActivity.java
+++ app/src/main/java/org/techtown/letseat/passwordActivity.java
@@ -3,6 +3,9 @@ package org.techtown.letseat;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.appbar.MaterialToolbar;
 
 public class passwordActivity extends AppCompatActivity {
 
@@ -10,5 +13,14 @@ public class passwordActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_password);
+
+        MaterialToolbar toolbar = findViewById(R.id.topMain);
+
+        toolbar.setNavigationOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
     }
 }
